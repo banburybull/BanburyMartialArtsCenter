1d616e3e65319d02c8636d00d8cd7cad194f5b36
David Strachan <banburybull.software@gmail.com>
2025-10-12 15:01:26 +0100

    expo 54 update

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {initializeAuth, getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
@@ -25,7 +24,5 @@ export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
  });
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
-
 // Export both auth and db
 export { db };
